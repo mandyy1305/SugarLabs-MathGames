37ffa5fc71c1b9dfe477adc1aa8ad0112e54b98e
Manish S <manishsuresh13@gmail.com>
2025-03-12 12:12:28 +0530

    add: fifteen puzzle

--- Godot/MainMenu/MainMenu.gd
+++ Godot/MainMenu/MainMenu.gd
@@ -4,13 +4,16 @@ func _ready():
 	# Connect button signals
 	$UI/VBoxContainer/BrokenCalculatorButton.connect("pressed", _on_broken_calculator_pressed)
 	$UI/VBoxContainer/FourColorMapButton.connect("pressed", _on_four_color_map_pressed)
-
+	$UI/VBoxContainer/FifteenPuzzle.connect("pressed", _on_fifteen_puzzle_pressed)
 func _on_broken_calculator_pressed():
 	# Change to the Broken Calculator scene
 	get_tree().change_scene_to_file("res://BrokenCalculator/BrokenCalculator.tscn")
 
 func _on_four_color_map_pressed():
 	get_tree().change_scene_to_file("res://FourColorTheory/FourColorGame.tscn")
+	
+func _on_fifteen_puzzle_pressed():
+	get_tree().change_scene_to_file("res://FifteenPuzzle/FifteenPuzzle.tscn")
 	
 	# Hide notification after 2 seconds
 	var tween = create_tween()
